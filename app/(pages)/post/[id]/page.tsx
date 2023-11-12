4cc0c04a99456caafbc408f8e0eef510c87442d5
kai <kindkai624@gmail.com>
2023-11-12 20:32:01 +0800

    fetch data test

--- app/(pages)/post/[id]/page.tsx
+++ app/(pages)/post/[id]/page.tsx
@@ -1,41 +1,20 @@
-/*import React from 'react';
-
-const fetchPost = async (postId: string) => {
-    const post = await fetch(`http://localhost:3000/api/posts/${postId}`, {
+//server
+async function getPostById(postId: string) {
+    const response = await fetch(`http://localhost:3000/api/posts/${postId}`, {
         method: 'GET',
     });
-    return post.json();
-};
-//Matadata
-export async function generateMetadata({ params }: any) {
-    const { post } = await fetchPost(params.id);
+    return response.json();
+}
 
- return {
-        metadataBase: new URL('http://localhost:3000'),
-        title: post.title,
-        description: post.description,
-        openGraph: {
-            title: post.title,
-            description: post.description,
-            url: `/posts/${params.id}`,
-            siteName: 'post',
-            images: [
-                {
-                    url: post.img_url,
-                    width: 1200,
-                    height: 800,
-                },
-            ],
-            type: 'website',
-        },
-    };
-}*/
+//
+const PostID = async ({ params, searchParams }: any) => {
+    const { post } = await getPostById(params.id);
+    console.log(post);
 
-const PostID = ({ params, searchParams }: any) => {
     return (
         <div>
-            <h1>Post: {params.id}</h1>
-            <h1>searchParams: {searchParams.new}</h1>
+            <h1>{post?.title}</h1>
+            <p>{post?.description}</p>
         </div>
     );
 };
